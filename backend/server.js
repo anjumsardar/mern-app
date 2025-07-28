c4a5ee0444245a32c19e81a0d8c91d7fc729f11b
anjumsardar <anjumsardar@gmail.com>
2025-07-28 06:43:52 +0500

    Updated

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
+import dotenv from 'dotenv'
+dotenv.config()
 import express from "express";
 import cors from 'cors'
-import dotenv from 'dotenv'
+
+
+
 import routers from './routes/routes.js'
 import dbCon from "./utlis/db.js";
 
 
 
-dotenv.config()
+
 const app=express()
 dbCon()
 app.use(cors())
@@ -15,7 +19,8 @@ app.use('/api',routers)
 
 
 
-const port = process.env.PORT
+const port = process.env.PORT || 8000
+
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on http://0.0.0.0:${port}`);
 });
--- backend/utlis/db.js
+++ backend/utlis/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
-
+//import dotenv from 'dotenv';  // Import dotenv
+//dotenv.config();  
+//const DB_URL=process.env.DB_URL || 'mongodb://localhost:27017/crud'
 const dbCon=async()=>{
     try {
-        await mongoose.connect(process.env.DB_URL)
+        await mongoose.connect('mongodb://localhost:27017/crud')
         console.log('Mongodb is conntected')
     } catch (error) {
         console.log(error)
